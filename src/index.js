7536f9f54d4e0356bc4de526ca55009371c9ecd0
DenVIII <Denis226@gmail.com>
2024-04-01 13:39:27 +0300

    Rewrite class "Task"

--- src/index.js
+++ src/index.js
@@ -1,50 +1,41 @@
 class Task {
-    static #_instances = []
+  constructor(title, dueDate, id, description = "-") {
+    this.title = title
+    this.dueDate = dueDate
+    this.id = id
+    this.description = description
+  }
 
-    constructor(data) {
-        Object.assign(this, data);
-        Task.#_instances.push(this);
-      }
-    
-      static get instances() { 
-        return Array.from(Task.#_instances);    // I use Array.from() for security measures - it returns copy of #_instances, 
-      }                                         // not the original one.
-    
-      static createNewTask(title, id, description, dueDate) {
-        new Task({title, id, description, dueDate})
-      }
+  getTitle() {
+    return this.title
+  }
 
-      static deleteTask(id) {
-        for (let i = 0, n = Task.#_instances.length; i < n; i++) {
-          const task = Task.#_instances[i]
-          if (task.id === id) {
-            Task.#_instances.splice(i, 1)
-            return
-          }
-        }
-      }
+  getDueDate() {
+    return this.dueDate
+  }
+
+  getId() {
+    return this.id
+  }
+
+  getDescription() {
+    return this.description
+  }
+
+  setTitle(title) {
+    this.title = title
+  }
+
+  setDueDate(dueDate) {
+    this.dueDate = dueDate
+  }
+
+  setDescription(description) {
+    this.description = description
+  }
 }
 
-const newTask = new Task({
-    title: "First Task",
-    id: "12f",
-    description: "Just random text for testing",
-    dueDate: "05.12.24"
-})
+const newTask = new Task ("First task (second try)", "01.04.24", "12f")
 
-const newTask2 = new Task({
-    title: "Second Task",
-    id: "15f",
-    description: "Just random text for testing",
-    dueDate: "01.12.24"
-})
-
-Task.createNewTask("random", "13a", "task", "07.12.24")
-
-console.log(Task.instances)
-console.log(newTask.title)
-console.log(newTask2)
-
-Task.deleteTask("15f")
-
-console.log(Task.instances)
+console.log(newTask.getTitle())
+console.log(newTask)
