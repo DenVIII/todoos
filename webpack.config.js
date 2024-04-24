21c3f51b9936f2953fb38ce9b0c0c643fa7794f4
DenVIII <Denis226@gmail.com>
2024-04-24 14:11:57 +0300

    Split style.css into modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todoos - your organizer-app',
+      template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name]-[hash][ext][query]',
+    clean: true,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        
+    ],
   },
 };
